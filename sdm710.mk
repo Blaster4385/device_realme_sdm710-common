5ac0179678f0698d4b673172eece9b38f242086d
Mathieu Chartier <mathieuc@google.com>
2018-06-25 04:07:02 +0200

    sdm710-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: AkaruiAikara <vall@aikara.me>

--- sdm710.mk
+++ sdm710.mk
@@ -391,6 +391,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext \
