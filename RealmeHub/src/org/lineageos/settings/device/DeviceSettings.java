568075f7448ca5a4eb6791198f184d09fa3e7f5a
AashiQ <77391250+aashiq-exe@users.noreply.github.com>
2021-09-05 00:07:55 +0530

    realme hub

---
+++ RealmeHub/src/org/lineageos/settings/device/DeviceSettings.java
@@ -0,0 +1,134 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.util.Log;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import com.ssos.support.preferences.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.TwoStatePreference;
+
+import org.lineageos.settings.preferences.SecureSettingListPreference;
+
+public class DeviceSettings extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CATEGORY_GRAPHICS = "graphics";
+    public static final String KEY_OTG_SWITCH = "otg";
+    public static final String KEY_GAME_SWITCH = "game";
+    public static final String PREF_KEY_FPS_INFO = "fps_info";
+    public static final String KEY_DND_SWITCH = "dnd";
+    public static final String KEY_CABC = "cabc";
+    public static final String CABC_SYSTEM_PROPERTY = "persist.cabc_profile";
+
+    public static final String KEY_SETTINGS_PREFIX = "device_setting_";
+
+    private static TwoStatePreference mOTGModeSwitch;
+    private static TwoStatePreference mGameModeSwitch;
+    private static NotificationManager mNotificationManager;
+    public static TwoStatePreference mDNDSwitch;
+    private SecureSettingListPreference mCABC;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        addPreferencesFromResource(R.xml.main);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mOTGModeSwitch = (TwoStatePreference) findPreference(KEY_OTG_SWITCH);
+        mOTGModeSwitch.setEnabled(OTGModeSwitch.isSupported());
+        mOTGModeSwitch.setChecked(OTGModeSwitch.isCurrentlyEnabled(this.getContext()));
+        mOTGModeSwitch.setOnPreferenceChangeListener(new OTGModeSwitch());
+
+        mGameModeSwitch = (TwoStatePreference) findPreference(KEY_GAME_SWITCH);
+        mGameModeSwitch.setEnabled(GameModeSwitch.isSupported());
+        mGameModeSwitch.setChecked(GameModeSwitch.isCurrentlyEnabled(this.getContext()));
+        mGameModeSwitch.setOnPreferenceChangeListener(new GameModeSwitch(getContext()));
+
+        SwitchPreference fpsInfo = (SwitchPreference) findPreference(PREF_KEY_FPS_INFO);
+        fpsInfo.setChecked(prefs.getBoolean(PREF_KEY_FPS_INFO, false));
+        fpsInfo.setOnPreferenceChangeListener(this);
+
+        mDNDSwitch = (TwoStatePreference) findPreference(KEY_DND_SWITCH);
+        mDNDSwitch.setChecked(prefs.getBoolean(KEY_DND_SWITCH, false));
+        mDNDSwitch.setOnPreferenceChangeListener(this);
+
+        mCABC = (SecureSettingListPreference) findPreference(KEY_CABC);
+        mCABC.setValue(Utils.getStringProp(CABC_SYSTEM_PROPERTY, "0"));
+        mCABC.setSummary(mCABC.getEntry());
+        mCABC.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+            case PREF_KEY_FPS_INFO:
+                boolean enabled = (Boolean) value;
+                if (enabled) {
+                    Utils.startService(this.getContext(), org.lineageos.settings.FPSInfoService.class);
+                } else {
+                    Utils.stopService(this.getContext(), org.lineageos.settings.FPSInfoService.class);
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        if (preference == mCABC) {
+            mCABC.setValue((String) value);
+            mCABC.setSummary(mCABC.getEntry());
+            Utils.setStringProp(CABC_SYSTEM_PROPERTY, (String) value);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
