4742b567f6c6f479009a1124a44f48d780a97ef8
Adam Lawson <adamjlawson22@gmail.com>
2021-11-06 07:47:04 +0000

    sdm710-common: Build tinyxml
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/mm-pp-dpps": library "libtinyxml.so" not found: needed by main executable

--- sdm710.mk
+++ sdm710.mk
@@ -439,6 +439,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
