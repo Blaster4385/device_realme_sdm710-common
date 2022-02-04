aa4c87bfe5c0c4029942a96d05e59718b3ea8b8c
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-02-04 21:19:54 +0530

    Revert "sdm710-common: Update bt stack from LA.UM.9.1.r1-09600-SMxxx0.0"
    This reverts commit eb5f85184c96510091d2492b1823f5331d7aee05.

--- sdm710.mk
+++ sdm710.mk
@@ -132,17 +132,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0 \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1 \
-    android.hardware.bluetooth.audio@2.1 \
-    audio.bluetooth.default \
-    com.dsi.ant@1.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    libbluetooth_audio_session \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-
-
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
